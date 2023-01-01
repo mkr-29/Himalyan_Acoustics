20370626999c4fcd0fa5bd8110470e887e9c446f
Manas Bajpai <97174936+MacWeTT@users.noreply.github.com>
2023-01-01 11:50:27 +0530

    landing carousel complete

--- JS/script.js
+++ JS/script.js
@@ -2,33 +2,66 @@
 const slides = document.querySelectorAll('.slide');
 const btnPrev = document.querySelector('.btn-prev');
 const btnNext = document.querySelector('.btn-next');
+const dotContainer = document.querySelector('.dots');
 
+const maxSlide = slides.length;
+let curSlide = 0;
 var counter = 0;
-slides.forEach(
-  (slide, index) => {
-    slide.style.bottom = `${index * 100}%`
-  }
-);
 
-const slideImage = function () {
-  slides.forEach(
-    (slide) => {
-      slide.style.transform = `translateY(${counter * 100}%)`
-    }
-  )
+slides.forEach((s, i) => (s.style.transform = `translateY(${100 * i}%)`));
+
+
+// Dot working logic
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML('beforeend', `<button class = "dots__dot" data-slide = "${i}"></button>`);
+  })
 };
+createDots();
 
-const goNext = function () {
-  counter++;
-  slideImage();
-}
-btnNext.addEventListener('click', goNext);
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+  };
+});
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove
+      ('dots__dot--active'));
 
-const goPrev = function () {
-  counter--;
-  slideImage();
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
 }
-btnPrev.addEventListener('click', goPrev);
+activateDot(0);
+
+
+// Slider Logic
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => (s.style.transform = `translateY(${100 * (i - slide)}%)`));
+
+};
+btnNext.addEventListener('click', function () {
+  if (curSlide == maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+});
+
+btnPrev.addEventListener('click', function () {
+  if (curSlide == 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+});
 
 
 //Product Slider
@@ -36,6 +69,7 @@ const productSlides = document.querySelectorAll('.product-slide');
 const productBtnPrev = document.querySelector('.product-btn-prev');
 const productBtnNext = document.querySelector('.product-btn-next');
 
+const prodMaxSlide = productSlides.length
 
 var productSlideCounter = 0;
 
@@ -53,13 +87,21 @@ const prodSlideImage = function () {
   )
 };
 const goProdNext = function () {
-  productSlideCounter++;
+  if (productSlideCounter != prodMaxSlide - 1) {
+    productSlideCounter++;
+  } else {
+    productSlideCounter = 0;
+  }
   prodSlideImage();
 }
 productBtnNext.addEventListener('click', goProdNext);
 
 const goProdPrev = function () {
-  productSlideCounter--;
+  if (productSlideCounter != 0) {
+    productSlideCounter--;
+  } else {
+    productSlideCounter = prodMaxSlide - 1;
+  }
   prodSlideImage();
 }
 productBtnPrev.addEventListener('click', goProdPrev);
