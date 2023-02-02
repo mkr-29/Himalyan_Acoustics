f08ed0472ad0e5a5d623a40f616ee5afddfc16df
Manas Bajpai <97174936+MacWeTT@users.noreply.github.com>
2023-02-02 19:31:17 +0530

    Better Scroll to top

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,27 @@ window.addEventListener('load', () => {
   });
 });
 
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#FF2C3A ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
 
-
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
 
 
 // Landing Slider
